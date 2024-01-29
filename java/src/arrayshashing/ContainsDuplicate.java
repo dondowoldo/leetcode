065e8c254deca7f4f8fa132188f20e817715df3e
Daniel Valasek <daniel.valasek@yahoo.com>
2024-01-29 22:21:32 +0100

    init arrayshashing pkg

---
+++ java/src/arrayshashing/ContainsDuplicate.java
@@ -0,0 +1,9 @@
+package arrayshashing;
+
+// https://leetcode.com/problems/contains-duplicate/description/
+
+public class ContainsDuplicate {
+    public static void main(String[] args){
+    System.out.println("hello, world");
+    }
+}
