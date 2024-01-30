ae3e0cc0390b77830389ea567b1d61db00624e84
Daniel Valasek <daniel.valasek@yahoo.com>
2024-01-30 13:41:57 +0100

    solve TwoSum

---
+++ java/src/arrayshashing/TwoSum.java
@@ -0,0 +1,25 @@
+package arrayshashing;
+
+import java.util.Arrays;
+
+public class TwoSum {
+  public static void main(String[] args) {
+    int[] nums = new int[] {2,5,5,11};
+    int target = 10;
+
+    System.out.println(Arrays.toString(twoSum(nums, target)));
+  }
+
+  private static int[] twoSum(int[] nums, int target) {
+    int index = 0;
+    while (index < nums.length) {
+      for(int i = index + 1; i < nums.length; i++) {
+        if (nums[i] + nums[index] == target) {
+          return new int[]{index, i};
+        }
+      }
+      index++;
+    }
+    return new int[0];
+  }
+}
