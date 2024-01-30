246097aa3323b3ac15114696a892f28223dfafdc
Daniel Valasek <daniel.valasek@yahoo.com>
2024-01-30 23:00:28 +0100

    add product of array cls

---
+++ java/src/arrayshashing/ProductOfArrayExceptSelf.java
@@ -0,0 +1,17 @@
+package arrayshashing;
+
+// https://leetcode.com/problems/product-of-array-except-self/description/
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+  public static void main(String[] args) {
+    int[] nums = new int[] {1, 2, 3, 4};
+    System.out.println(Arrays.toString(productExceptSelf(nums)));
+  }
+
+  private static int[] productExceptSelf(int[] nums) {
+
+    return null;
+  }
+}
